913602dbde55e263b979548a4991f46f2a83c813
Pushpender Singh <52063297+pushpinder6805@users.noreply.github.com>
2025-10-27 20:12:07 +0530

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,32 +1,42 @@
 # name: discourse-mailgun
-# about: Discourse Plugin for Mailgun webhooks 
+# about: Discourse Plugin for Mailgun webhooks
 # version: 0.1
 # authors: Pushpender Chaudhary
-# url: hhttps://github.com/pushpinder6805/tracks-mailgun
+# url: https://github.com/pushpinder6805/tracks-mailgun
 
 require 'openssl'
 
+enabled_site_setting :discourse_base_url
+enabled_site_setting :discourse_api_key
+enabled_site_setting :discourse_api_username
+
 after_initialize do
+  # Make Mailgun API key visible in UI
+  SiteSetting::Definition.find { |s| s.setting == :mailgun_api_key }&.tap do |s|
+    s.client = true
+    s.hidden = false
+    s.category = 'email'
+  end
+
   module ::DiscourseMailgun
     class Engine < ::Rails::Engine
       engine_name "discourse-mailgun"
       isolate_namespace DiscourseMailgun
 
       class << self
-        # signature verification filter
         def verify_signature(timestamp, token, signature, api_key)
           digest = OpenSSL::Digest::SHA256.new
           data = [timestamp, token].join
           hex = OpenSSL::HMAC.hexdigest(digest, api_key, data)
-
           signature == hex
         end
 
-        # posting the email through the discourse api
         def post(url, params)
-          Excon.post(url,
-            :body => URI.encode_www_form(params),
-            :headers => { "Content-Type" => "application/x-www-form-urlencoded" })
+          Excon.post(
+            url,
+            body: URI.encode_www_form(params),
+            headers: { "Content-Type" => "application/x-www-form-urlencoded" }
+          )
         end
       end
     end
@@ -38,32 +48,25 @@ after_initialize do
     before_action :verify_signature
 
     def incoming
-      mg_body    = params['body-plain']
-      mg_subj    = params['subject']
-      mg_to      = params['To']
-      mg_from    = params['From']
-      mg_date    = params['Date']
-
       m = Mail::Message.new do
-        to      mg_to
-        from    mg_from
-        date    mg_date
-        subject mg_subj
-        body    mg_body
+        to      params['To']
+        from    params['From']
+        date    params['Date']
+        subject params['subject']
+        body    params['body-plain']
       end
 
-      handler_url = SiteSetting.discourse_base_url + "/admin/email/handle_mail"
+      handler_url = "#{SiteSetting.discourse_base_url}/admin/email/handle_mail"
+      payload = {
+        'email' => m.to_s,
+        'api_key' => SiteSetting.discourse_api_key,
+        'api_username' => SiteSetting.discourse_api_username
+      }
 
-      params = {'email'        => m.to_s,
-                'api_key'      => SiteSetting.discourse_api_key,
-                'api_username' => SiteSetting.discourse_api_username}
-      ::DiscourseMailgun::Engine.post(handler_url, params)
-
+      ::DiscourseMailgun::Engine.post(handler_url, payload)
       render plain: "done"
     end
 
-    # we mark this controller as an API
-    # in order to skip CSRF and other discourse filters
     def is_api?
       true
     end
@@ -71,18 +74,22 @@ after_initialize do
     private
 
     def verify_signature
-      unless ::DiscourseMailgun::Engine.verify_signature(params['timestamp'], params['token'], params['signature'], SiteSetting.mailgun_signing_key)
-        render json: {}, :status => :unauthorized
+      unless ::DiscourseMailgun::Engine.verify_signature(
+        params['timestamp'],
+        params['token'],
+        params['signature'],
+        SiteSetting.mailgun_api_key
+      )
+        render json: {}, status: :unauthorized
       end
     end
   end
-
 
   DiscourseMailgun::Engine.routes.draw do
     post "/incoming" => "mailgun#incoming"
   end
 
   Discourse::Application.routes.append do
-    mount ::DiscourseMailgun::Engine, at: "mailgun"
+    mount ::DiscourseMailgun::Engine, at: "/mailgun"
   end
 end
